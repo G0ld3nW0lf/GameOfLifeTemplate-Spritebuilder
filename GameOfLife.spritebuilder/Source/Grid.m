d9d455de7ddf9b7c5d34ebcb39705971a601e391
Shashank Guduru <shashank.guduru@gmail.com>
2014-06-24 22:39:18 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -133,7 +133,7 @@ static const int GRID_COLUMNS = 10;
                     }
                 }
             }
-            //NSLog(@"%d ", currentCreature.livingNeighbors);
+            NSLog(@"%d ", currentCreature.livingNeighbors);
         }
     }
     //NSLog(@"counted");
