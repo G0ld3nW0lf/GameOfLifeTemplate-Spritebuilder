e12dcd53f4cff839194608558f2dfd7b16756315
Shashank Guduru <shashank.guduru@gmail.com>
2014-06-24 22:16:46 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/Grid.m
+++ GameOfLife.spritebuilder/Source/Grid.m
@@ -148,7 +148,7 @@ static const int GRID_COLUMNS = 10;
     //update each Creature's neighbor count
     [self countNeighbors];
     
-    //[self printArray];
+    [self printArray];
     //update each Creature's state
     [self updateCreatures];
     
